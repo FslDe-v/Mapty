9992394b3f3773b7145993fa8e3a50a55c0d319f
“faisal <faisalsamialshalan>
2025-02-15 19:43:53 +0300

    Second-commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+// Using the Geolocation API
+
+// navigator.geolocation.getCurrentPosition(
+//   function (position) {
+//     console.log(position);
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
+//     console.log(latitude, longitude);
+//     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//   },
+//   function () {
+//     alert('An erroe occurred while trying to get your location');
+//   }
+// );
